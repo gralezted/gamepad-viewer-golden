96caddf59c7f2fcd63d1fd67d092341dcc2d5a5a
gralezted <gralezted@gmail.com>
2026-09-06 18:07:10 -0700

    Add index.html and complete Vercel deployment setup

---
+++ js/gamepad-modified.js
@@ -0,0 +1,15 @@
+// Paste this modified section into js/gamepad.js line 74-79
+// Change from:
+//     dualsense: {
+//         id: /0ce6/,
+//         name: "DualSense",
+//         colors: ["white", "black"],
+//         triggers: true,
+//     },
+// To:
+dualsense: {
+    id: /0ce6/,
+    name: "DualSense",
+    colors: ["white", "black", "pink"],
+    triggers: true,
+},
